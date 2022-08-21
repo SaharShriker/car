10b2a8e83418ef9538ca84a93506ebbd9e74bafb
SaharShriker <95046499+SaharShriker@users.noreply.github.com>
2022-08-21 13:07:17 -0400

    start working on the service

--- src/main/java/com/car/services/CarService.java
+++ src/main/java/com/car/services/CarService.java
@@ -3,6 +3,8 @@ package com.car.services;
 import com.car.beans.Car;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,7 +19,17 @@ public class CarService {
     private final String url = "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q=";
     private final List<Car> results;
 
-    
+    public List<Car> getByIdPlate(Long number){
+        results.clear();
+        String data = restTemplate.getForObject(url+number,String.class);
+        JSONArray records = new JSONObject(data).getJSONObject("result").getJSONArray("records");
+
+
+
+
+
+        return null;
+    }
 
 
 
